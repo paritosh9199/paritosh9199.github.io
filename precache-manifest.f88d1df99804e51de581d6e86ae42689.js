323706046bb37587a7279123b8079440a93330ab
paritosh9199 <paritoshsrivastava9199@gmail.com>
2020-06-08 02:16:09 +0530

    V4

---
+++ precache-manifest.f88d1df99804e51de581d6e86ae42689.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc58e00fa0c7885208cf83e1f7d2d54a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf2c681ead3373afd5ee",
+    "url": "/static/css/main.5d9fe57e.chunk.css"
+  },
+  {
+    "revision": "efffd2610aa422c22c0d",
+    "url": "/static/js/2.781ae2de.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.781ae2de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf2c681ead3373afd5ee",
+    "url": "/static/js/main.0ba4432d.chunk.js"
+  },
+  {
+    "revision": "504a74b7eaa1fe616c39",
+    "url": "/static/js/runtime-main.084c52ba.js"
+  },
+  {
+    "revision": "2295ce2d70df2d887683aa68dc1dbaa3",
+    "url": "/static/media/libryed.2295ce2d.png"
+  },
+  {
+    "revision": "f5dafffe6a6c654b975b86df53dba85c",
+    "url": "/static/media/pong.f5dafffe.png"
+  },
+  {
+    "revision": "a0b8656d0a1e5bdc67d330fc7997808e",
+    "url": "/static/media/rikscha.a0b8656d.png"
+  }
+]);
